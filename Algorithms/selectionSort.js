8ffdee0fd8492a72a48a4b93ce3a5b87375dd5ee
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-09-10 12:37:43 +0100

    Tri par sélection

---
+++ Algorithms/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  console.log(arr);
+}
+
+let arr = [8, 5, 3, 1, 7, 2, 4, 6];
+
+selectionSort(arr);
